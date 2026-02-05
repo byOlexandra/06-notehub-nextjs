ff8d6f31b57435d7ef6aa30c9b4104ad62b3e5c9
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-05 08:55:32 +0200

    Add components

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,68 @@
-// import css from './NotesPage.module.css'
+import css from "./NotesPage.module.css";
+import NoteList from "@/components/NoteList/NoteList";
+import SearchBox from "../SearchBox/SearchBox";
+import NoteForm from "../NoteForm/NoteForm";
+import Modal from "../Modal/Modal";
+import Pagination from "../Pagination/Pagination";
+import Loader from "../Loader/Loader";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNotes } from "../../services/noteService";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import { keepPreviousData } from "@tanstack/react-query";
+// import toast, { Toaster } from "react-hot-toast";
 
 export default function Notes() {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [currentPage, setCurrentPage] = useState(1);
+    const debouncedSetSearchQuery = useDebouncedCallback((value: string) => {
+        setSearchQuery(value)
+        setCurrentPage(1);
+    }, 300);
+    const [isOpenModal, setIsOpenModal] = useState(false);
+
+    const openModal = () => setIsOpenModal(true);
+    const closeModal = () => setIsOpenModal(false);
+
+    const { data, isFetching, isError } = useQuery({
+        queryKey: ["notes", searchQuery, currentPage],
+        queryFn: () => fetchNotes(searchQuery, currentPage),
+        placeholderData: keepPreviousData,
+    });
+
+    useEffect(() => {
+        if (isError) toast.error('Somehing bad happenned. Try again.')
+    }, [isError])
+
+    const notes = data?.notes || [];
+    const totalPages = data?.totalPages || 0;
+
     return (
-        <p>Notes</p>
-    )
+        <div className={css.app}>
+            <header className={css.toolbar}>
+                <SearchBox text={searchQuery} onSearch={debouncedSetSearchQuery} />
+                {totalPages > 0 && (
+                    <Pagination
+                        changePage={setCurrentPage}
+                        page={currentPage}
+                        totalPg={totalPages}
+                    />
+                )}
+                <button className={css.button} onClick={openModal}>
+                    Create note +
+                </button>
+            </header>
+            {/* <Toaster /> */}
+            {isFetching && <Loader />}
+            {notes.length > 0 && (
+                <NoteList notes={notes} />
+            )}
+            {isOpenModal && (
+                <Modal onClose={closeModal}>
+                    <NoteForm onClose={closeModal} />
+                </Modal>
+            )}
+        </div>
+    );
 }
 
---
+++ components/NoteList/NoteList.tsx
@@ -0,0 +1,42 @@
+import css from "./NoteList.module.css";
+import type { Note } from "../../types/note";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "@/lib/api";
+// import toast from "react-hot-toast";
+
+interface NoteListProps {
+    notes: Note[];
+}
+
+export default function NoteList({ notes }: NoteListProps) {
+    const queryClient = useQueryClient();
+
+    const { mutate: deleteMutate } = useMutation({
+        mutationFn: deleteNote,
+        onSuccess() {
+            toast.success('You successfully deleted a note!');
+            queryClient.invalidateQueries({ queryKey: ['notes'] });
+        },
+    });
+
+    return (
+        <ul className={css.list}>
+            {notes.map((note) => (
+                <li key={note.id} className={css.listItem}>
+                    <h2 className={css.title}>{note.title}</h2>
+                    <p className={css.content}>{note.content}</p>
+                    <div className={css.footer}>
+                        <span className={css.tag}>{note.tag}</span>
+                        <button
+                            type="button"
+                            className={css.button}
+                            onClick={() => deleteMutate(note.id)}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
+}
